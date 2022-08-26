2c79215193d84559c1f7f53d56c05b835c090f27
Arian Hosseini <arianhosseini@outlook.com>
2022-08-26 04:44:11 +0430

    chore: added Contact route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
 
 				<nav className="nav">
 					<Link to="/books">Books Page</Link> | {"    "}
-					<Link to="/about">About us</Link>
+					<Link to="/about">About us</Link> | {"    "}
+					<Link to="/contact">Contact us</Link>
 				</nav>
 			</header>
 		</div>
